1a07a9b6b1de025d7c49ad1ede2fe7f04b1e913a
suhyeon <suhyeon3484@naver.com>
2024-07-29 11:59:12 +0900

    set: Graphql 설정 변경

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { createYoga, YogaInitialContext } from 'graphql-yoga';
-import { GraphQLSchemaHost } from '@nestjs/graphql';
-import { Request } from 'express';
-
-// Context 타입 정의
-interface Context extends YogaInitialContext {
-  req: Request;
-}
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.init();
 
-  const graphqlSchemaHost = app.get(GraphQLSchemaHost);
-  const { schema } = graphqlSchemaHost;
-
-  // GraphQL Yoga 설정
-  const yoga = createYoga<Context>({
-    schema,
-    context: ({ req }) => ({ req }),
-  });
-
-  app.use('/graphql', yoga);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   await app.listen(3000);
 }
